fab19185d6c93e860f98eff0a031ec76233e7bed
zeloras <zeloras@gmail.com>
2014-01-23 06:58:15 +0400

    Update language file (rus version)

--- application/classes/modules/feedback/i18n/ru.php
+++ application/classes/modules/feedback/i18n/ru.php
@@ -4,4 +4,9 @@ return array(
     'Email letters to come' => 'Почта на которую будут приходить пиьсма',
     'Maximum number of characters in a letter' => 'Максимальное кол-во символов в сообщении',
     'Message format' => 'Формат сообщения',
+    'Feedback' => 'Обратная связь',
+    'Feedback settings' => 'Настройка обратной связи',
+    'Acces to view' => 'Доступ для просмотра',
+    'Change settings' => 'Смена настроек',
+    'Send feedback' => 'Отправка писем'
 );
